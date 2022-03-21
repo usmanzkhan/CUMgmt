d26054a2280ed3a825761b70d2706e71ece61e18
usmanzkhan <usmanzkhan@s3zlimied.co.k>
2022-03-21 08:53:36 +0000

    update requirements, add reqs

--- City_University/settings.py
+++ City_University/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'City_University.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'City_University.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'cityuniversity',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
